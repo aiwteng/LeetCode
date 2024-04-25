41c75096fcb673e14ad27b56f4426460d4b54e90
wtengg <116067858+aiwteng@users.noreply.github.com>
2024-04-26 01:26:49 +0800

    Update 001_Two_Sum.py

--- Easy/001_Two_Sum.py
+++ Easy/001_Two_Sum.py
@@ -5,8 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (i!=j and nums[i]+nums[j] == target):
-                    return [i,j]
-        return[]
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return[i,j]
+
+        # using hashmap
+        hashmap = {} # value : index
+
+        for i, n in enumerate(nums): # i = index, n = number in array
+            diff = target - n
+
+            if diff in hashmap:
+                return [hashmap[diff],i]
+            hashmap[n] = i # update the value and index into hashmap
+        return
+
+        
